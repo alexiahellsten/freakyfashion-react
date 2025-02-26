3aa7b447c19188e0d67d191a4312792ae2280e8d
Alexia Hellsten <alexia.hellsten@gmail.com>
2025-02-26 21:48:26 +0100

    Add Navbar component

--- client/src/components/Navbar/NavIcons.jsx
+++ client/src/components/Navbar/NavIcons.jsx
@@ -6,11 +6,11 @@ function NavIcons() {
   return (
     <div className="flex items-center">
       <Button variant="ghost" size="icon" className="text-foreground">
-        <Heart className="h-5 w-5" fill="currentColor" />
+        <Heart fill="currentColor" />
       </Button>
       <Link to="/basket">
         <Button variant="ghost" size="icon" className="text-foreground">
-          <ShoppingCart className="h-5 w-5" />
+          <ShoppingCart />
         </Button>
       </Link>
     </div>
