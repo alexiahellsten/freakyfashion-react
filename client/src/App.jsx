32680e34fe629ae8e20154e55b039f9173aa1c38
Alexia Hellsten <alexia.hellsten@gmail.com>
2025-02-27 12:42:01 +0100

    Add spots

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router";
 // import { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Spots from "./components/Spots/Spots";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <main className="flex flex-col justify-center gap-2.5">
           <Hero />
+          <Spots className="hidden lg:visible" />
         </main>
       </BrowserRouter>
     </>
