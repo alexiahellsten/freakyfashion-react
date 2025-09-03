2c3356f939d8fa36f1f49b05386758c8e1f1e103
Alexia Hellsten <alexia.hellsten@gmail.com>
2025-09-03 12:19:05 +0200

    Add dotenv

--- server/src/routes/favorites.js
+++ server/src/routes/favorites.js
@@ -2,6 +2,14 @@ import express from "express";
 const router = express.Router();
 import db from "../../db/db.js";
 
+// Middleware för att kontrollera att användaren är inloggad
+function requireLogin(req, res, next) {
+  if (!req.session.userId) {
+    return res.status(401).json({ message: "Obehörig användare" });
+  }
+  next();
+}
+
 // GET /api/favorites + api/favorites?q=
 router.get("/", function (req, res, next) {
   const searchQuery = req.query.q; 
