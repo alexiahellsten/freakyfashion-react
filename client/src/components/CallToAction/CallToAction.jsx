341853dbcb5cc85870c49400b64f5ca8cd57de52
Alexia Hellsten <alexia.hellsten@gmail.com>
2025-09-05 17:26:01 +0200

    Add CTA component to Favorites page

--- client/src/components/CallToAction/CallToAction.jsx
+++ client/src/components/CallToAction/CallToAction.jsx
@@ -3,7 +3,9 @@ function CallToAction() {
     <div className="bg-black m-auto p-4 text-center text-white text-sm flex justify-between">
       <span>Kundservice</span>
       <span>Fri frakt för medlemmar när du shoppar för minst 399:-</span>
-      <span>Logga in</span>
+
+      {/* TODO: Ersätt detta med en länk till registreringssidan */}
+      <span>Registrera</span>
     </div>
   );
 }
--- client/src/pages/Favorites/Favorites.jsx
+++ client/src/pages/Favorites/Favorites.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import ProductGrid from "../../components/ProductGrid/ProductGrid";
 import Footer from "../../components/Footer/Footer";
+import CallToAction from "../../components/CallToAction/CallToAction";
 
 const API_URL = "http://localhost:8000";
 
@@ -35,6 +36,7 @@ function Favorites() {
   return (
     <>
       <header>
+        <CallToAction />
         <Navbar />
       </header>
       
