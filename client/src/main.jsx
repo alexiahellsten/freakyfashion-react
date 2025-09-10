deeff1216604c78f6d574329f3a31124a9253d00
Alexia Hellsten <alexia.hellsten@gmail.com>
2025-09-10 18:59:48 +0200

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,11 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./App.jsx";
-import { BasketProvider } from "./contexts/BasketContext";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
-      <BasketProvider>
-        <App />
-      </BasketProvider>
+      <App />
     </StrictMode>
   </BrowserRouter>
 );
