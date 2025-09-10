8164f5b6cbe02e31bb9401889097a842e512a85b
Alexia Hellsten <alexia.hellsten@gmail.com>
2025-09-10 15:15:07 +0200

    Update CTA

--- client/src/components/CallToAction/CallToAction.jsx
+++ client/src/components/CallToAction/CallToAction.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router";
+
 function CallToAction() {
   return (
     <div className="bg-black m-auto p-4 text-center text-white text-sm flex justify-between">
-      <span>Kundservice</span>
+      <span >Kundservice</span>
       <span>Fri frakt för medlemmar när du shoppar för minst 399:-</span>
 
       {/* TODO: Ersätt detta med en länk till registreringssidan */}
-      <span>Registrera</span>
+      <Link to="/register">
+       Registrera
+      </Link>
     </div>
   );
 }
