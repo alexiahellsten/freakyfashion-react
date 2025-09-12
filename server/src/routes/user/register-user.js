ac9427493747bf51cf1292e5e368881ae1bc4bd5
Alexia Hellsten <alexia.hellsten@gmail.com>
2025-09-12 11:29:54 +0200

    Separate user routes

---
+++ server/src/routes/user/register-user.js
@@ -0,0 +1,40 @@
+import express from "express";
+const router = express.Router();
+import db from "../../../db/db.js";
+
+// POST /register - Skapa en ny användare
+router.post("/", (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = db.prepare("SELECT * FROM users WHERE email = ?").get(email);
+    if (existingUser) {
+      return res.status(400).json({ 
+        message: "Användaren finns redan" });
+    }
+    
+    const result = db.prepare("INSERT INTO users (email, password) VALUES (?, ?)").run(email, password);
+    const newUser = db.prepare("SELECT * FROM users WHERE id = ?").get(result.lastInsertRowid);
+
+    // Spara den nya användaren i sessionen
+    req.session.userId = newUser.id;
+    req.session.email = newUser.email;
+    req.session.isAdmin = newUser.isAdmin;
+    
+
+    res.status(201).json({ 
+      message: "Registreringen lyckades",
+       user: { 
+        id: newUser.id, 
+        email: newUser.email,
+        isAdmin: newUser.isAdmin 
+      } });
+      
+    console.log("Registering user:", email);
+    console.log("User created:", newUser);
+    
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default router;
